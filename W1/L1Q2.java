a0c4fb58a63152258ab489e67939b7182837cc27
Abqary Nasution <abqaryne@gmail.com>
2025-10-29 15:55:19 +0800

    Add Files
    Lab files for each question

---
+++ W1/L1Q2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fop_l1;
+
+/**
+ *
+ * @author abqary1907
+ */
+public class L1Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Name:       Muhammad Abqary Nasution");
+        System.out.println("Matric:     24216602 ");
+        System.out.println("Adress:     Kuala Lumpur, Malaysia ");
+        System.out.println("Email:      24216602@siswa.um.edu.my");
+        System.out.println("Contact:    014-3773173");
+        System.out.println("Programme:  Bachelor Of Computer Science (Information Systems)");
+    }
+    
+}
